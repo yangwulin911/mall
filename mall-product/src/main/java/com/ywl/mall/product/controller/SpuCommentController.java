d9f33e2b8cf45926aae18f9e9d14a23efbc39290
yangwulin <yangwulin911@163.com>
2020-06-02 01:09:42 +0800

    init

---
+++ mall-product/src/main/java/com/ywl/mall/product/controller/SpuCommentController.java
@@ -0,0 +1,89 @@
+package com.ywl.mall.product.controller;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ywl.mall.product.entity.SpuCommentEntity;
+import com.ywl.mall.product.service.SpuCommentService;
+import com.ywl.common.utils.PageUtils;
+import com.ywl.common.utils.R;
+
+
+
+/**
+ * 商品评价
+ *
+ * @author yangwulin
+ * @email yangwulin911@163.com
+ * @date 2020-06-01 00:44:18
+ */
+@RestController
+@RequestMapping("product/spucomment")
+public class SpuCommentController {
+    @Autowired
+    private SpuCommentService spuCommentService;
+
+    /**
+     * 列表
+     */
+    @RequestMapping("/list")
+    //@RequiresPermissions("product:spucomment:list")
+    public R list(@RequestParam Map<String, Object> params){
+        PageUtils page = spuCommentService.queryPage(params);
+
+        return R.ok().put("page", page);
+    }
+
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{id}")
+    //@RequiresPermissions("product:spucomment:info")
+    public R info(@PathVariable("id") Long id){
+		SpuCommentEntity spuComment = spuCommentService.getById(id);
+
+        return R.ok().put("spuComment", spuComment);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    //@RequiresPermissions("product:spucomment:save")
+    public R save(@RequestBody SpuCommentEntity spuComment){
+		spuCommentService.save(spuComment);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    //@RequiresPermissions("product:spucomment:update")
+    public R update(@RequestBody SpuCommentEntity spuComment){
+		spuCommentService.updateById(spuComment);
+
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    //@RequiresPermissions("product:spucomment:delete")
+    public R delete(@RequestBody Long[] ids){
+		spuCommentService.removeByIds(Arrays.asList(ids));
+
+        return R.ok();
+    }
+
+}
